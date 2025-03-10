8211a2c50a259cc856eda90d7d5e80dcbe0a2cea
MustafaBatuhan <lelekasika135@outlook.com>
2025-03-10 23:16:14 +0300

    Core katmanın daki sınıflar eklendi ve düzenlendi.

---
+++ CSProjeDemo1.Core/Entities/Uye.cs
@@ -0,0 +1,44 @@
+﻿using CSProjeDemo1.Core.Enums;
+using CSProjeDemo1.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjeDemo1.Core.Entities
+{
+    public class Uye : IUye
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int UyeNumarasi { get; set; }
+        public List<Kitap> OduncAldigiKitaplar { get; set; }
+
+        public Uye(string ad, string soyad, int uyeNumarasi)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            UyeNumarasi = uyeNumarasi;
+            OduncAldigiKitaplar = new List<Kitap>();
+        }
+
+        public void KitapOduncAl(Kitap kitap)
+        {
+            if (kitap.KitapDurumu == Durum.OduncAlabilir)
+            {
+                kitap.KitapDurumu = Durum.OduncVerildi;
+                OduncAldigiKitaplar.Add(kitap);
+            }
+        }
+
+        public void KitapIadeEt(Kitap kitap)
+        {
+            if (OduncAldigiKitaplar.Contains(kitap))
+            {
+                kitap.KitapDurumu = Durum.OduncAlabilir;
+                OduncAldigiKitaplar.Remove(kitap);
+            }
+        }
+    }
+}

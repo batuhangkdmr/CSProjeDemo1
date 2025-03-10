1f840e027047e50d9d06fdfce290d97ed97cd83c
MustafaBatuhan <lelekasika135@outlook.com>
2025-03-10 22:52:24 +0300

    Add project files.

---
+++ CSProjeDemo1.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSProjeDemo1.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

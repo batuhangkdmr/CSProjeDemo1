837c0c21ee215175a9ccea3814ac2d5cce2cff2c
MustafaBatuhan <lelekasika135@outlook.com>
2025-03-11 20:06:56 +0300

    Service Katmanın daki sınıflar eklendi ve düzenlendi.

---
+++ CSProjeDemo1.Service/Services/KitapService.cs
@@ -0,0 +1,59 @@
+﻿using CSProjeDemo1.Core.Entities;
+using CSProjeDemo1.Core.Enums;
+using CSProjeDemo1.Data.IRepositories;
+using CSProjeDemo1.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjeDemo1.Service.Services
+{
+    public class KitapService : IKitapService
+    {
+        private readonly IRepository<Kitap> _kitapRepository;
+
+        public KitapService(IRepository<Kitap> kitapRepository)
+        {
+            _kitapRepository = kitapRepository;
+        }
+
+        public async Task<IEnumerable<Kitap>> GetAllBooksAsync()
+        {
+            return await _kitapRepository.GetAllAsync();
+        }
+
+        public async Task<IEnumerable<Kitap>> GetAvailableBooksAsync()
+        {
+            return await _kitapRepository.FindAsync(k => k.KitapDurumu == Durum.OduncAlabilir);
+        }
+
+        public async Task<Kitap> GetBookByIdAsync(int id)
+        {
+            return await _kitapRepository.GetByIdAsync(id);
+        }
+
+        public async Task<bool> OduncAlAsync(int kitapId, int uyeId)
+        {
+            var kitap = await _kitapRepository.GetByIdAsync(kitapId);
+            if (kitap == null || kitap.KitapDurumu != Durum.OduncAlabilir)
+                return false;
+
+            kitap.KitapDurumu = Durum.OduncVerildi;
+            await _kitapRepository.UpdateAsync(kitap);
+            return true;
+        }
+
+        public async Task<bool> IadeEtAsync(int kitapId)
+        {
+            var kitap = await _kitapRepository.GetByIdAsync(kitapId);
+            if (kitap == null || kitap.KitapDurumu != Durum.OduncVerildi)
+                return false;
+
+            kitap.KitapDurumu = Durum.OduncAlabilir;
+            await _kitapRepository.UpdateAsync(kitap);
+            return true;
+        }
+    }
+}
---
+++ CSProjeDemo1.Service/Services/UyeService.cs
@@ -0,0 +1,37 @@
+﻿using CSProjeDemo1.Core.Entities;
+using CSProjeDemo1.Data.IRepositories;
+using CSProjeDemo1.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjeDemo1.Service.Services
+{
+    public class UyeService : IUyeService
+    {
+        private readonly IRepository<Uye> _uyeRepository;
+
+        public UyeService(IRepository<Uye> uyeRepository)
+        {
+            _uyeRepository = uyeRepository;
+        }
+
+        public async Task<IEnumerable<Uye>> GetAllUyelerAsync()
+        {
+            return await _uyeRepository.GetAllAsync();
+        }
+
+        public async Task<Uye> GetUyeByIdAsync(int id)
+        {
+            return await _uyeRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Kitap>> GetOduncAldigiKitaplarAsync(int uyeId)
+        {
+            var uye = await _uyeRepository.GetByIdAsync(uyeId);
+            return uye?.OduncAldigiKitaplar ?? new List<Kitap>();
+        }
+    }
+}
